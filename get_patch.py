c6ab04fdba78a752774e10f20276edf2d41bddfd
SachitShroff <sachit.shroff@gmail.com>
2019-10-01 17:26:39 -0700

    initial commit with starter code

---
+++ get_patch.py
@@ -0,0 +1,102 @@
+import keras
+import keras.backend as K
+
+from conv_model import conv_model_no_color_adjust
+
+
+def load_gtsrb(n_channel=3, train_file_name=None):
+    """
+    Load GTSRB data as a (datasize) x (channels) x (height) x (width) numpy
+    matrix. Each pixel is rescaled to lie in [0,1].
+    """
+
+    def load_pickled_data(file, columns):
+        """
+        Loads pickled training and test data.
+
+        Parameters
+        ----------
+        file    : string
+                          Name of the pickle file.
+        columns : list of strings
+                          List of columns in pickled data we're interested in.
+
+        Returns
+        -------
+        A tuple of datasets for given columns.
+        """
+
+        with open(file, mode='rb') as f:
+            dataset = pickle.load(f)
+        return tuple(map(lambda c: dataset[c], columns))
+
+    def preprocess(x, n_channel):
+        """
+        Preprocess dataset: turn images into grayscale if specified, normalize
+        input space to [0,1], reshape array to appropriate shape for NN model
+        """
+
+        if n_channel == 3:
+            # Scale features to be in [0, 1]
+            x = (x / 255.).astype(np.float32)
+        else:
+            # Convert to grayscale, e.g. single Y channel
+            x = 0.299 * x[:, :, :, 0] + 0.587 * x[:, :, :, 1] + \
+                0.114 * x[:, :, :, 2]
+            # Scale features to be in [0, 1]
+            x = (x / 255.).astype(np.float32)
+            x = x[:, :, :, np.newaxis]
+        return x
+
+    # Load pickle dataset
+    if train_file_name is None:
+        x_train, y_train = load_pickled_data(
+            DATA_DIR + 'train.p', ['features', 'labels'])
+    else:
+        x_train, y_train = load_pickled_data(
+            DATA_DIR + train_file_name, ['features', 'labels'])
+    x_val, y_val = load_pickled_data(
+        DATA_DIR + 'valid.p', ['features', 'labels'])
+    x_test, y_test = load_pickled_data(
+        DATA_DIR + 'test.p', ['features', 'labels'])
+
+    # Preprocess loaded data
+    x_train = preprocess(x_train, n_channel)
+    x_val = preprocess(x_val, n_channel)
+    x_test = preprocess(x_test, n_channel)
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+def get_patch(X, pos):
+    """
+    Crop all samples in <X> to make a set of patches <X_cropped>
+    at the same position <pos>
+    """
+
+    top, bot, left, right = pos
+    X_cropped = np.zeros((X.shape[0], bot - top, right - left, X.shape[3]))
+    for i, x in enumerate(X):
+        X_cropped[i] = np.copy(x[top:bot, left:right, :])
+    return X_cropped
+
+
+model = conv_model_no_color_adjust()
+model.load_weights("./keras_weights/stn_v5.hdf5")
+
+get_stn_output = K.function([model.layers[0].input, K.learning_phase()],
+                            [model.layers[1].output])
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_gtsrb()
+
+# Define cropping positions
+pos_S = (7, 24, 0, 11)
+pos_T = (7, 23, 8, 17)
+pos_O = (6, 23, 13, 24)
+pos_P = (6, 23, 22, 32)
+
+# Need to rescale input to [-1, 1] for STN
+X_stn = get_stn_output([X_train, 0])[0] * 0.5 + 0.5
+S = get_patch(X_stn, pos_S)
+T = get_patch(X_stn, pos_T)
+O = get_patch(X_stn, pos_O)
+P = get_patch(X_stn, pos_P)
